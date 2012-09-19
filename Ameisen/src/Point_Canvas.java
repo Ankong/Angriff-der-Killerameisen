26c28381096c7b95b93229efc9d3d9181253ba6e
blackearth7 <oranienbuger@gmail.com>
2012-09-19 21:30:13 +0200

    fertige Darstellung der Punkte (int Rot) und aller Linien (in grau)

--- Ameisen/src/Point_Canvas.java
+++ Ameisen/src/Point_Canvas.java
@@ -10,11 +10,21 @@ public class Point_Canvas extends Component {
 	}
 	
 	public void paint (Graphics g){
-		int width = 7;
-		int high = 7;
-		g.setColor(Color.red);
+		int width = 6;
+		int high = 6;
 		for (int i = 0; i < Datei_oeffnen.list.size(); i++) {
-			g.fillOval((Datei_oeffnen.list.get(i).getxPos()/3), (Datei_oeffnen.list.get(i).getyPos()/3), width, high);
+			try {
+				for (int j = 0; j < Datei_oeffnen.list.size(); j++) {
+					g.setColor(Color.lightGray);
+					g.drawLine(((Datei_oeffnen.list.get(i).getxPos()/4)+3),((Datei_oeffnen.list.get(i).getyPos()/4)+3), ((Datei_oeffnen.list.get(j).getxPos()/4)+3), ((Datei_oeffnen.list.get(j).getyPos()/4)+3));
+				}
+			} catch (IndexOutOfBoundsException e){
+				//System.out.println("geht nicht");
+			}
+		}
+		for (int i = 0; i < Datei_oeffnen.list.size(); i++) {
+			g.setColor(Color.red);
+			g.fillRect((Datei_oeffnen.list.get(i).getxPos()/4), (Datei_oeffnen.list.get(i).getyPos()/4), width, high);
 		}
 	}
 }
