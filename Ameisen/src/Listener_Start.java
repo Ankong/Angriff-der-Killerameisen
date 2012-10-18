363cbfd1fc73085512e13488a821bb48523e826e
phausmann <Masterpatt@web.de>
2012-10-18 09:44:16 +0200

    Die gelaufene Strecke der jeweils letzten Iteration wird jetzt gezeichnet. Ein zeichnen der Strecke in jeder einzelnen Iteration ist mit dem, was wir zur Zeit haben nich möglich. Ich versuche das noch zu fixen. Außerdem: zufälliges Setzen der Ameisen nach jeder Iteration auf eine neue Stadt.

--- Ameisen/src/Listener_Start.java
+++ Ameisen/src/Listener_Start.java
@@ -36,6 +36,11 @@ public class Listener_Start implements ActionListener {
 		
 		
 		for (int b = 0; b < TSP_Algorithmus.v_Iteration; b++) {
+			//Liste leeren und neue Ameisen für neue zufällige Städte erstellen und setzen, nach dem ersten Durchlauf
+			if (!TSP_Algorithmus.antList.isEmpty() & b!=0) {
+				TSP_Algorithmus.antList.clear();
+				TSP_Algorithmus.ameisen_generieren();
+			}
 			for (int l = 0; l < TSP_Algorithmus.antList.size(); l++) {
 				gesamtLaenge = 0;
 				if (!TSP_Algorithmus.antList.get(l).tabuList.isEmpty()) {
@@ -54,16 +59,12 @@ public class Listener_Start implements ActionListener {
 						break;
 					}	
 				}
-				GUI.draw_TSP();
-				GUI.frame_refresh();
 				TSP_Algorithmus.antList.get(l).setGesamtlaenge(Math.round((100 *gesamtLaenge))/100.0);
 				
 				for (int a = 0; a < TSP_Algorithmus.antList.get(l).tabuList.size(); a++) {
 					update = TSP_Algorithmus.pheromonUpdate(l, TSP_Algorithmus.antList.get(l).tabuList.get(a));
 					TSP_Algorithmus.antList.get(l).tabuList.get(a).setPheromon(TSP_Algorithmus.antList.get(l).tabuList.get(a).getPheromon()+update);
-				}	
-				
-					
+				}					
 			}
 			TSP_Algorithmus.v_init_Pheromon = (1 - TSP_Algorithmus.v_Verdunst) * TSP_Algorithmus.v_init_Pheromon;
 			GUI.draw_TSP();
