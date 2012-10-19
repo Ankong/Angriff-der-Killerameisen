0345c6531b9a55a8a1048389f9347fc1e7cbf33a
phausmann <Masterpatt@web.de>
2012-10-19 12:52:38 +0200

    Formel für das Auwählen der Städte dazu! Scheint auch zu funktionieren :)

--- Ameisen/src/TSP_Algorithmus.java
+++ Ameisen/src/TSP_Algorithmus.java
@@ -21,11 +21,11 @@ public class TSP_Algorithmus {
 	public static int v_Ameisen;
 	public static int v_Stadte;
 	public static int v_Iteration;
-	public static int v_Pheromon;
-	public static int v_heuristisch;
+	public static double v_Pheromon;
+	public static double v_heuristisch;
 	public static double v_Verdunst;
 	public static double v_init_Pheromon;
-	public static int v_heur_Pheromon;
+	public static double v_heur_Pheromon;
 	static boolean posible;
 	/**
 	 * Einlesen der Parameter aus den Textfeldern
@@ -35,11 +35,11 @@ public class TSP_Algorithmus {
 		v_Ameisen = Integer.parseInt(GUI.t_Ameisen.getText());
 		v_Stadte = Integer.parseInt(GUI.t_Stadte.getText());
 		v_Iteration = Integer.parseInt(GUI.t_Iteration.getText());
-		v_Pheromon = Integer.parseInt(GUI.t_Pheromon.getText());
-		v_heuristisch = Integer.parseInt(GUI.t_heuristisch.getText());
+		v_Pheromon = Double.parseDouble(GUI.t_Pheromon.getText());
+		v_heuristisch = Double.parseDouble(GUI.t_heuristisch.getText());
 		v_Verdunst = (double)(GUI.s_Verdunst.getValue())/100;
 		v_init_Pheromon = Double.parseDouble(GUI.t_init_Pheromon.getText());
-		v_heur_Pheromon = Integer.parseInt(GUI.t_heur_Pheromon.getText());
+		v_heur_Pheromon = Double.parseDouble(GUI.t_heur_Pheromon.getText());
 	}
 	
 	/**
@@ -106,7 +106,7 @@ public class TSP_Algorithmus {
 	}
 	
 	public static Double pheromonUpdate (int index, TSP_Strecke strecke) {
-		int qparam = v_heur_Pheromon;
+		double qparam = v_heur_Pheromon;
 		double update;
 		if (antList.get(index).tabuList.contains(strecke)) {
 			update = qparam / antList.get(index).getGesamtlaenge();
@@ -116,5 +116,37 @@ public class TSP_Algorithmus {
 		}
 		return update;
 	}
-	
+	public TSP_Strecke Stadtauswahl(int ameisenid, double startx, double starty) {
+		TSP_Strecke strecke = null;
+		double auswahl = Integer.MAX_VALUE;
+		
+		for (int k= 0; k < streckenList.size(); k++) {
+			if ( (startx == streckenList.get(k).getStartxPos() ) && ( starty == streckenList.get(k).getStartyPos() ) ) {
+				for (int i = k; k <= v_Stadte + k; i++) {
+					posible = ( TSP_Ameisen.check_posibility(ameisenid, streckenList.get(i).getEndxPos(), streckenList.get(i).getEndyPos()) ) ;
+					if ( ( berechne(i, ameisenid, startx, starty) < auswahl ) && (posible) ) {
+						auswahl = berechne(i, ameisenid, startx, starty);
+						strecke = streckenList.get(k);
+					}
+				}
+			break;
+			}
+		}
+		return strecke;
+	}
+	public double berechne (int index, int amid, double beginnx, double beginny) {
+		double wert1, wert2, gesamtwert;
+		wert2 = 0;
+		boolean moeglich;
+		
+		wert1 = (Math.pow(streckenList.get(index).getPheromon(), v_Pheromon)) * (Math.pow(streckenList.get(index).getLaenge(), (-v_heuristisch)));
+		for (int j = index; j<= v_Stadte + index; j++) {
+			moeglich = ( TSP_Ameisen.check_posibility(amid, streckenList.get(j).getEndxPos(), streckenList.get(j).getEndyPos()) ) ;
+			if (moeglich = true) {
+				wert2 += (Math.pow(streckenList.get(index).getPheromon(), v_Pheromon)) * (Math.pow(streckenList.get(index).getLaenge(), (-v_heuristisch)));
+			}
+		}
+		gesamtwert = wert1/wert2;
+		return gesamtwert;
+	}
 }
