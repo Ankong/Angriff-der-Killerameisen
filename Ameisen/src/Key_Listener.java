850b9eb325ec5e70ebe8bbd1cefba66ba195ff6a
Sebastian <Sebastian@Netbook.Speedport_W_723V_1_26_000>
2012-10-24 18:26:39 +0200

    Key_listener class für textfeld eingaben kontrolle

---
+++ Ameisen/src/Key_Listener.java
@@ -0,0 +1,42 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JOptionPane;
+
+import com.sun.org.apache.bcel.internal.generic.GETFIELD;
+
+
+public class Key_Listener implements KeyListener{
+	
+    private static final char[] NUMERIC_CHARS = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\b'
+    };
+    
+    private String keyset;
+    
+	public boolean checkInput(char C){
+        boolean result = false;
+		for(int b = 0; (b < NUMERIC_CHARS.length) && (!result); b++)
+            if(C == NUMERIC_CHARS[b])
+                result = true;
+		return(result);
+	}
+	
+	//Tastendruck abfangen und überprüfen ob Eingabe valide ist   
+	 public void keyPressed(KeyEvent arg0){
+
+	    }
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		if (checkInput(arg0.getKeyChar()) == false){
+			arg0.consume();
+			JOptionPane.showMessageDialog(null, "Die Eingabe ist Fehlerhaft. Bitte geben sie eine Zahl ein.", "Achtung", JOptionPane.OK_CANCEL_OPTION);
+        }
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+
+	}
+}
+
+
